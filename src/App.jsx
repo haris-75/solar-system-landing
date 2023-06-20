086a9cd1c916f2d15861583a3473a3019b7742a6
haris-75 <haris.ejaz@datumbrain.com>
2023-06-20 13:18:34 +0500

    Add TailWindCss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import './App.css';
+import HeaderText from './HeaderText';
+
+function App() {
+  return (
+    <div className='max-w-[1440px]'>
+      <HeaderText />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HeaderText.jsx
@@ -0,0 +1,12 @@
+export default function HeaderText() {
+  return (
+    <div>
+      <div className='font-satoshi font-bold text-[80px] text-center '>
+        <div>Design tools</div>
+        <div className=' text-2xl'>from the future</div>
+      </div>
+      <p>Unleash your creativity with Diagrams </p>
+      <p> AI-powered design tools.</p>
+    </div>
+  );
+}
